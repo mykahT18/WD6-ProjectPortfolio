92dac23c057f7707ff95c3e8b26b585581ea708c
carlosjunod <carlos@junod.cl>
2017-07-15 20:37:35 -0400

    adding basic configuration to pyment process - WIP

--- Teamwork/wd6university/app/AppKernel.php
+++ Teamwork/wd6university/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Flosch\Bundle\StripeBundle\FloschStripeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- Teamwork/wd6university/src/AppBundle/Controller/CartController.php
+++ Teamwork/wd6university/src/AppBundle/Controller/CartController.php
@@ -7,8 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\Entity\courses;
-use AppBundle\Entity\Users;
+use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Session\Session;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 /**
  * Cart Controller
@@ -40,7 +42,7 @@ class CartController extends Controller
 
 
         return $this->redirect('/');
-    
+
     }
      /**
      * @Route("/", name="cart_view")
@@ -48,30 +50,23 @@ class CartController extends Controller
     public function cartAction(Request $request)
     {
     	$session = $request->getSession();
-        // $session->clear();
 
     	$cartList = $session->get('cart');
         // var_dump($cartList);
     	$total = 0.00;
     	$i = 0;
     	foreach ($cartList as $value) {
-
     		$results[$value] = $this->getDoctrine()->getRepository('AppBundle:courses')->findOneBy(array('id' => $value));
-
-
-    		// $total += $results->price;
     	}
 
-        foreach ($results as $item) {
-            $total += $item->getPrice();
-        }
+      foreach ($results as $item) {
+          $total += $item->getPrice();
+      }
 
-            // var_dump($results);
+      $session->set('total', $total);
 
-    		// get_object_var($results);
-
-        $url = $_SERVER['REQUEST_URI'];
-        return $this->render('pages/cart.html.twig', array('url' => $url, 'results' => $results, 'total' => $total));
+      $url = $_SERVER['REQUEST_URI'];
+      return $this->render('pages/cart.html.twig', array('url' => $url, 'results' => $results, 'total' => $session->get('total') ) );
     }
     /**
      * @Route("/del/{id}", name="cart_delete")
@@ -83,6 +78,48 @@ class CartController extends Controller
     	unset($cartList[$id]);
         var_dump($cartList);
          return $this->redirectToRoute('cart_view');
+    }
+
+
+    /**
+     * @Route("/checkout", name="checkout")
+     */
+    public function checkoutAction(Request $request){
+
+      var_dump($request->get('total'));
+      return $this->render('pages/checkout.html.twig');
+
+
+    }
+
+    /**
+     * @Route("/charge", name="charge")
+     */
+    public function charge(Request $request){
+      $stripeClient = $this->get('flosch.stripe.client');
+
+      /**
+       * $chargeAmount (int)              : The charge amount in cents, for instance 1000 for 10.00 (of the currency)
+       * $chargeCurrency (string)         : The charge currency (for instance, "eur")
+       * $paymentToken (string)           : The payment token obtained using the Stripe.js library
+       * $stripeAccountId (string|null)   : (optional) The connected string account ID, default null (--> charge to the platform)
+       * $applicationFee (int)            : The amount of the application fee (in cents), default to 0
+       * $chargeDescription (string)      : (optional) The charge description for the customer
+       */
+
+
+        $chargeAmount = $request->get('total');
+        $chargeCurrency = 'USD';
+        $paymentToken = $request->get('StripeToken');
+        // $stripeAccountId = "ca_B1FLHhIXQiD5QkHDwHbF9Os5emNjIHUN";
+        $chargeDescription = 'Payment TEST';
+
+
+
+      $stripeClient->createCharge($chargeAmount, $chargeCurrency, $paymentToken, $chargeDescription);
+
+      var_dump($request->get('total'));
+
     }
 
 }
