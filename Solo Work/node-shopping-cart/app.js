22b8b55e47a62b0f983e4c4b561aa35c138a9c12
Mykah <mykah.taylor@gmail.com>
2017-07-24 23:41:32 -0400

    Register new user is working

--- Solo Work/node-shopping-cart/app.js
+++ Solo Work/node-shopping-cart/app.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 // var users = require('./routes/users');
 
 var app = express();
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +26,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- Solo Work/node-shopping-cart/routes/index.js
+++ Solo Work/node-shopping-cart/routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var Grocery = require('../models/groceries');
+var csrf = require('csurf');
+var passport = require('passport');
 
+var csrfProtection = csrf();
+// All routes should be protected
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -14,5 +19,20 @@ router.get('/', function(req, res, next) {
 router.get('/addCart/:id', function(req, res, next){
 	res.render('error');
 })
+router.get('/addFav/:id', function(req, res, next){
+	res.render('error');
+})
+router.get('/user/signup', function(req, res, next){
+	res.render('user/signup', { csrfToken: req.csrfToken() });
+})
+router.post('/user/signup', passport.authenticate('local.signup', {
+	successRedirect: '/user/profile',
+	failureRedirect: '/user/signup',
+	failureFlash: true
+}));
+router.get('/user/profile', function(req, res, next){
+	res.render('user/profile');
+})
+
 
 module.exports = router;
