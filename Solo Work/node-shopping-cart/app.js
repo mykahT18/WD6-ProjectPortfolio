f337d6856452edb85dab9112e9c32fc42bafcb91
Mykah <mykah.taylor@gmail.com>
2017-07-25 14:06:04 -0400

    Signin and signup and protected routes done

--- Solo Work/node-shopping-cart/app.js
+++ Solo Work/node-shopping-cart/app.js
@@ -8,9 +8,10 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var index = require('./routes/index');
-// var users = require('./routes/users');
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('localhost:27017/shopping');
@@ -25,6 +26,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
@@ -32,6 +34,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+	res.locals.login = req.isAuthenticated();
+	next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', index);
 
 
--- Solo Work/node-shopping-cart/routes/index.js
+++ Solo Work/node-shopping-cart/routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Grocery = require('../models/groceries');
-var csrf = require('csurf');
-var passport = require('passport');
 
-var csrfProtection = csrf();
-// All routes should be protected
-router.use(csrfProtection);
+var Grocery = require('../models/groceries');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -22,17 +18,5 @@ router.get('/addCart/:id', function(req, res, next){
 router.get('/addFav/:id', function(req, res, next){
 	res.render('error');
 })
-router.get('/user/signup', function(req, res, next){
-	res.render('user/signup', { csrfToken: req.csrfToken() });
-})
-router.post('/user/signup', passport.authenticate('local.signup', {
-	successRedirect: '/user/profile',
-	failureRedirect: '/user/signup',
-	failureFlash: true
-}));
-router.get('/user/profile', function(req, res, next){
-	res.render('user/profile');
-})
-
 
 module.exports = router;
