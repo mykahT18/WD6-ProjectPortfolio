b23cbfed35a434ace4d2ddfa565d716d85ebb0b6
Mykah <mykah.taylor@gmail.com>
2017-07-26 14:39:34 -0400

    Adding items to cart and displaying them on cart page

--- Solo Work/node-shopping-cart/routes/index.js
+++ Solo Work/node-shopping-cart/routes/index.js
@@ -31,5 +31,11 @@ router.get('/addCart/:id', function(req, res, next){
 router.get('/addFav/:id', function(req, res, next){
 	res.render('error');
 })
-
+router.get('/cart', function(req, res, next){
+	if(!req.session.cart){
+		return res.render('shop/cart', {products: null});
+	}
+	var cart = new Cart(req.session.cart);
+	res.render('shop/cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
 module.exports = router;
--- Solo Work/node-shopping-cart/routes/user.js
+++ Solo Work/node-shopping-cart/routes/user.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var Grocery = require('../models/groceries');
 var csrf = require('csurf');
 var passport = require('passport');
 
-var csrfProtection = csrf();
+// Importing Model
+var Grocery = require('../models/groceries');
+
 // All routes should be protected
+var csrfProtection = csrf();
 router.use(csrfProtection);
 
+// Get Profile page
 router.get('/profile', isLoggedIn, function(req, res, next){
 	res.render('user/profile');
 });
-
+// Get Logout Route
 router.get('/logout', function(req, res, next){
 	req.logout();
 	res.redirect('/');
@@ -20,23 +23,23 @@ router.get('/logout', function(req, res, next){
 router.use('/', notLoggedIn, function(req, res, next){
 	next();
 });
-
+// Get signup page
 router.get('/signup', function(req, res, next){
 	var messages = req.flash('error');
 	res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors:messages.length > 0 });
 })
-
+// Post to signup page---- Registering a new user
 router.post('/signup', passport.authenticate('local.signup', {
 	successRedirect: '/user/profile',
 	failureRedirect: '/user/signup',
 	failureFlash: true
 }));
-
+// Get signin page
 router.get('/signin', function(req, res, next){
 	var messages = req.flash('error');
-	res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors:messages.length > 0 });
+	res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 });
-
+// Checking database if user is in database - then login
 router.post('/signin', passport.authenticate('local.signin', {
 	successRedirect: '/user/profile',
 	failureRedirect: '/user/signin',
